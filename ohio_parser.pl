fdfc558d550e28b210e478c7edf4f98b5e5fa13b
EconJoe <staudt.joe@gmail.com>
2016-07-21 12:59:08 -0400

    Added prisoner status

--- ohio_parser.pl
+++ ohio_parser.pl
@@ -7,11 +7,11 @@ use File::Slurp;
 
 
 my $listpath="B:\\Research\\Projects\\Incarceration\\Ohio\\data";
-my $inpath="B:\\Research\\Projects\\Incarceration\\Ohio\\html";
+my $inpath="B:\\Research\\Projects\\Incarceration\\Ohio\\ohio_html";
 my $outpath="B:\\Research\\Projects\\Incarceration\\Ohio\\data";
 
-open (OUTFILE_DATA, ">$listpath\\ohio_data_test.txt") or die "Can't open subjects file: ohio_data_test";
-print OUTFILE_DATA "offnum	name	dob	inst	county	date\n";
+open (OUTFILE_DATA, ">$listpath\\ohio_data_test.txt") or die "Can't open subjects file: ohio_data_test.txt";
+print OUTFILE_DATA "offnum	name	dob	inst	status	county	date\n";
 
 my @types=("A", "R", "W");
 foreach my $type (@types) {
@@ -21,14 +21,20 @@ foreach my $type (@types) {
     chomp($offnum);
     my $content = read_file("$inpath\\ohio_html_$offnum.txt");
     #print $text;
+    
+    #<span id="ctl00_ContentPlaceHolder1_Lbl_Status" class="coltext"><font color="Red">ESCAPED</font></span> 
+    #<span id="ctl00_ContentPlaceHolder1_Lbl_Status(.*)>ESCAPED<
+    #<span id="ctl00_ContentPlaceHolder1_Lbl_Status(.*)>(.*?)<
 
     # Parse DOB, County, and Admission Date
-    my $name; my $dob; my $inst; my $county; my $date;
+    my $name; my $dob; my $inst;  my $status; my $county; my $date;
     if ($content=~/<span id="ctl00_ContentPlaceHolder1_Lbl_FullName(.*?)>(.*?)<\/span>/) { $name=$2; }
     if ($content=~/<span id="ctl00_ContentPlaceHolder1_Lbl_DOB" class="coltext">(.*?)<\/span>/) { $dob=$1; }
     if ($content=~/<span id="ctl00_ContentPlaceHolder1_Lbl_Inst" class="coltext">(.*?)<\/span>/) { $inst=$1; }
+    if ($content=~/<span id="ctl00_ContentPlaceHolder1_Lbl_Status" class="coltext">(.*?)<\/span>/) { $status=$1; }
+    if ($content=~/<span id="ctl00_ContentPlaceHolder1_Lbl_Status" class="coltext"><font color="(.*?)">(.*?)<\/font><\/span>/) { $status=$2; }
     while ($content=~/Lbl_CommCnt" class="coltext">(.*?)<span>(.*?)<\/span>(.*?)Lbl_AdminDate" class="coltext">(.*?)<span>(.*?)<\/span>/gs) { $county=$2; $date=$5;
-        print OUTFILE_DATA "$offnum	$name	$dob	$inst	$county	$date\n";
+        print OUTFILE_DATA "$offnum	$name	$dob	$inst	$status	$county	$date\n";
     }
     
 
